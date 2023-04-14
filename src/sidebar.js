469f0f34e2fb9383bd1715f2bc2ca17fb8b12dd1
manish~ <manishumeshgupta3@gmail.com>
2023-04-15 03:45:09 +0530

    change

--- src/sidebar.js
+++ src/sidebar.js
@@ -110,7 +110,7 @@ const Sidebar = () => {
           </div>
         </CDBSidebarFooter>
       </CDBSidebar>
-      <div className="container" style={{height:'100vh',scrollbarWidth:'none', overflow:'scroll'}}>
+      <div className="container" style={{height:'100vh',width:'fit-content',overflowY:'scroll'}}>
         <div className="row vc">
           <div className="visual">
             <Visual count={count} />
